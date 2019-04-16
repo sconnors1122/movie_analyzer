78bcd1b90309e21e3e418d3152ed996f070e75d9
Shannon <sconnors1122@gmail.com>
2019-04-16 19:44:20 -0400

    added submission form

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
-class LoginForm(FlaskForm):
+
+class SubmissionForm(FlaskForm):
     title = StringField('Movie Title', validators=[DataRequired()])
-    review = PasswordField('Review', validators=[DataRequired()])
+    review = StringField('Review', validators=[DataRequired()])
     submit = SubmitField('Submit')
--- app/routes.py
+++ app/routes.py
@@ -1,28 +1,13 @@
 from flask import render_template, flash, redirect, url_for
 from app import app
-from app.forms import LoginForm
+from app.forms import SubmissionForm
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 def index():
-    user = {'username': 'Miguel'}
-    posts = [
-        {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
+    form = SubmissionForm()
     if form.validate_on_submit():
         flash('Login requested for user {}, remember_me={}'.format(
             form.username.data, form.remember_me.data))
         return redirect(url_for('index'))
-    return render_template('login.html', title='Sign In', form=form)
+    return render_template('index.html', title='Home', form=form)
